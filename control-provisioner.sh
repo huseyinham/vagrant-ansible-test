607dfba92b2ed63a41659b39a92feb10602cadf5
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2016-04-29 12:58:19 +0100

    updates provisioner to always install ansible

--- control-provisioner.sh
+++ control-provisioner.sh
@@ -7,7 +7,7 @@ sudo -u vagrant -H sh -c "sudo apt-get update"
 sudo -u vagrant -H sh -c "sudo apt-get install software-properties-common"
 sudo -u vagrant -H sh -c "sudo apt-add-repository ppa:ansible/ansible -y"
 sudo -u vagrant -H sh -c "sudo apt-get update"
-sudo -u vagrant -H sh -c "sudo apt-get install ansible -y"
+sudo -u vagrant -H sh -c "sudo apt-get install ansible -y --force-yes"
 
 sudo -u vagrant -H sh -c "mkdir ~/ansible-test"
 sudo -u vagrant -H sh -c "cp /vagrant/inventory ~/ansible-test/inventory"
