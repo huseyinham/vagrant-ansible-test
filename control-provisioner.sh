7481d67ae3ace36b1c1d4a7e3a8ab5f47d3f652e
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2016-04-29 09:25:01 +0100

    changes provisioner to install ansible on control machine

--- control-provisioner.sh
+++ control-provisioner.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 sudo -u vagrant -H sh -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa"
-sudo -u vagrant -H sh -c "mv ~/.ssh/id_rsa.pub /vagrant/id_rsa.pub"
+sudo -u vagrant -H sh -c "cp ~/.ssh/id_rsa.pub /vagrant/id_rsa.pub"
+
+sudo -u vagrant -H sh -c "sudo apt-get update"
+sudo -u vagrant -H sh -c "sudo apt-get install software-properties-common"
+sudo -u vagrant -H sh -c "sudo apt-add-repository ppa:ansible/ansible -y"
+sudo -u vagrant -H sh -c "sudo apt-get update"
+sudo -u vagrant -H sh -c "sudo apt-get install ansible -y"
+
+sudo -u vagrant -H sh -c "mkdir ~/ansible-test"
+sudo -u vagrant -H sh -c "cp /vagrant/inventory ~/ansible-test/inventory"
+
+
